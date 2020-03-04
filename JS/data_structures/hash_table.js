ca3c0f787fdc79511cae5c8e4548c0ed97962bdc
graemebrooks <graemebrooks@gmail.com>
2020-03-04 12:54:10 -0600

    adds keys and values methods to hash table

--- JS/data_structures/hash_table.js
+++ JS/data_structures/hash_table.js
@@ -30,6 +30,32 @@ class HashTable {
 		}
 		return undefined;
 	}
+	values() {
+		let valuesArr = [];
+		for (let i = 0; i < this.keyMap.length; i++) {
+			if (this.keyMap[i]) {
+				for (let j = 0; j < this.keyMap[i].length; j++) {
+					if (!valuesArr.includes(this.keyMap[i][j][1])) {
+						valuesArr.push(this.keyMap[i][j][1]);
+					}
+				}
+			}
+		}
+		return valuesArr;
+	}
+	keys() {
+		let keysArr = [];
+		for (let i = 0; i < this.keyMap.length; i++) {
+			if (this.keyMap[i]) {
+				for (let j = 0; j < this.keyMap[i].length; j++) {
+					if (!keysArr.includes(this.keyMap[i][j][0])) {
+						keysArr.push(this.keyMap[i][j][0]);
+					}
+				}
+			}
+		}
+		return keysArr;
+	}
 }
 
 let ht = new HashTable(17);
@@ -40,4 +66,6 @@ ht.set('salmon', '#FA8072');
 ht.set('lightcoral', '#F08080');
 ht.set('mediumvioletred', '#C71585');
 ht.set('plum', '#DDA0DD');
-console.log(ht.get('lightcoral'));
+ht.set('purple', '#DDA0DD');
+ht.set('violet', '#DDA0DD');
+console.log(ht.values());
